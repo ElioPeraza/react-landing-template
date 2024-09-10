d67811fcdade568b631e8b65726582ec0e613f23
ElioPeraza <Perazaelio@gmail.com>
2024-09-10 11:01:21 -0300

    Last update

--- src/Card.jsx
+++ src/Card.jsx
@@ -6,7 +6,7 @@ function CardBig() {
   return (
     <Card className='rounded m-5'style={{ backgroundColor: '#f8f9fa' }}>
       <Card.Body>
-        <Card.Title style={{ fontSize: '5rem' }}>A warm Welcome!</Card.Title>
+        <Card.Title style={{ fontSize: '5rem' }}>A Warm Welcome!</Card.Title>
         <Card.Text>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore officia quaerat
           blanditiis labore aspernatur vitae esse nostrum quis exercitationem ullam, 
